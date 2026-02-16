695af5dc6f70d90f06546578e753e547f3652e63
UpenderReddyThudi <upender7@gmail.com>
2026-02-17 01:23:41 +1100

    Update Chapter 02 changes

--- Chapter_02_Java_Concepts/05_var_let_const.js
+++ Chapter_02_Java_Concepts/05_var_let_const.js
@@ -4,20 +4,20 @@
 
 // Global Scope
 var a = 10; 
-console.log("Global Variable: a = ", a);
+console.log("Var Global Variable: a = ", a);
 
 // Defination of the function
 function printhellovar(){
-        console.log("Hello! I am learning variables in Java Script");
+        console.log("Hello! I am learning var in Java Script");
         var b = 20; // Local Scope
-        console.log("Local Variable: b = ",b);
+        console.log("Var Outer Black: b = ",b); // 20, var is function scoped, so it is accessible within the entire function
 
         if(true){
             var b = 30;
-            console.log("Block Variable: b = ",b); // 30
+            console.log("Var Inner Block: b = ",b); // 30, var is function scoped, so it does not create a new scope inside the block
         }
 
-        console.log("Block Variable: b = ",b);
+        console.log("Var Inner Block: b = ",b); //30, var is function scoped, so it does not create a new scope inside the block
 }
 
 // Calling of the function 
@@ -32,30 +32,30 @@ console.log("Variable Redeclaration: C = ", C); // 20
 
 // let - Block Scoped, can be updated but not re-declared
 let x = 10;
-console.log("Global Variable: x = ", x);
+console.log("Let Global Variable: x = ", x);
 
 function printhellolet(){
         console.log("Hello! I am learning let in Java Script");
         let y = 20;
-        console.log("Function Scoped : y = ",y);  //20
+        console.log("Let Outer Block: y = ",y);  //20 let is block scoped, so it is accessible only within the block it is defined in
 
         if(true){
             let y = 30; 
-            console.log("Block Scoped: y = ",y); // 30
+            console.log("Let Inner Block: y = ",y); // 30 // let is block scoped, so it creates a new scope inside the block, and this y is different from the y defined in the outer block
         }
-        console.log("Function Scoped: y = ",y); // 20
+        console.log("Let outer Block: y = ",y); // 20 // let is block scoped, so it is accessible only within the block it is defined in, and this y is the one defined in the outer block, not the one defined in the inner block
 }
       
 printhellolet();
 
 // let does not allow re-declaration:
 let Z = 10; 
-console.log("Let Variable : Z = ", Z); // 10
+console.log("Let Initial Value : Z = ", Z); // 10
 
 // let Z = 20; // Error: Identifier 'Z' has already been declared
 
 Z = 20; // Allowed, updating the value
-console.log("Let variable Update: Z = ", Z); // 20
+console.log("Let Updated Value: Z = ", Z); // 20
 
 // const - Block Scoped, cannot be updated or re-declared
 const PI = 3.14;
